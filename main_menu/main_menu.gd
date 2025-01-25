5024c6ccf567967c3b53b0d49dad44f62e82e6f4
Achilleas Ioannis Yfantis <achilleasein@gmail.com>
2025-01-25 20:02:09 +0200

    Main menu placeholder

---
+++ main_menu/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var start_button = $MenuHolder/MenuItems/Start
+
+func _ready() -> void:
+	$MenuHolder/MenuItems/Start.grab_focus()
+
+#func _process(delta: float) -> void:
+	#pass
+
+func _on_Start_pressed() -> void:
+	get_tree().change_scene_to_file("res://killing_floor/killing_floor.tscn")
+
+func _on_Settings_pressed() -> void:
+	var settings = load("res://assets/main_menu/characters.png").get_instance_id()
+	get_tree().current_scene.add_child(settings)
+
+func _on_Exit_pressed() -> void:
+	get_tree().quit()
