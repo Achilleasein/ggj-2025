50bd6162e14e46f1d1d6951ec1a30a8b155ce7e5
EvanKrav <evangeloskravaris@gmail.com>
2025-01-25 22:19:26 +0100

    Enemy and attacks

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var speed : float = 100.0 
 @export var fire_rate_range : Vector2 = Vector2(1.0, 3.0)
 @export var random_movement_offset : float = 0.5
-@export var bullet_scene_path : String = "res://Bubble_Main/bullet.tscn"
+@export var bullet_scene_path : String = "res://Bubble_Main/enemy_bullet.tscn"
 
 var player: Node2D
 var bullet_scene: PackedScene
